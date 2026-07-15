5354bf93a53e1fa2ee0681a036e2e0f6456a6d50
Jerquito <najle_10@hotmail.com>
2026-07-15 16:22:49 -0500

    Cambios de destacados confirmados

--- service-worker.js
+++ service-worker.js
@@ -2,12 +2,12 @@ const CACHE = "malystore-v1";
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll([
   "./",
   "./index.html",
-  "./css/style.css",
-  "./js/app.js",
-  "./data/productos.json",
-  "./img/logo.svg",
-  "./img/hero.svg",
-  "./img/Yape.jpg",
-  "./img/Plin.jpg"
+  "./Parte01_Pagina_principal/css/style.css",
+  "./Parte01_Pagina_principal/js/app.js",
+  "./Parte01_Pagina_principal/data/productos.json",
+  "./Parte01_Pagina_principal/img/logo.svg",
+  "./Parte01_Pagina_principal/img/hero.svg",
+  "./Parte01_Pagina_principal/img/Yape.jpg",
+  "./Parte01_Pagina_principal/img/Plin.jpg"
 ]))));
 self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
